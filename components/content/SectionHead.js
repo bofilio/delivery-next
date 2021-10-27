a8340da2da10afd34f8502ba0e65a3aeb2b6b35a
h.talha <h.talha@al-mouradia.dz>
2021-10-27 17:45:07 +0100

    add restaurant sections

---
+++ components/content/SectionHead.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SectionHead = ({title}) => {
+    return (
+        <div className="flex items-center mt-12 mb-8">
+            <h1 className=" text-2xl font-bold text-gray-900">{title}</h1>
+            <div className="flex-grow"></div>
+            <div className="flex right-0 items-center">
+                <a className=" text-base font-bold text-purple-700" href="#">see all </a>
+                <svg className="text-purple-700" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default SectionHead
