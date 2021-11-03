a0e18cd8267cd2b6b603c9a7d9efb16fd0b0fe23
bofilio <hemzatalha@gmail.com>
2021-11-03 20:31:26 +0100

    search page and search filter modal

--- components/layout/TopBar.js
+++ components/layout/TopBar.js
@@ -10,6 +10,10 @@ import ShopingBagIcon from '../icons/ShopingBagIcon'
 import LocationIcon from '../icons/LocationIcon'
 import { useRouter } from "next/router";
 import SearchIcon from '../icons/SearchIcon'
+import SortingIcon from '../icons/SortingIcon'
+import CashIcon from '../icons/CashIcon'
+import CartIcon from '../icons/CartIcon'
+import SearchFilterModal from '../modals/SearchFilterModal'
 
 let currentRoute ="/";
 const TopBar = () => {
@@ -58,21 +62,24 @@ const TopBar = () => {
 
                 </form>
                 <button className="lg:hidden flex items-center justify-center w-12 h-12 rounded-xl ml-4 p-3 bg-light" >
-                    <svg className="w-5 h-5" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M14.8571 7.42857C14.8571 3.32588 11.5313 0 7.42857 0C3.32588 0 0 3.32588 0 7.42857C0 11.5313 3.32588 14.8571 7.42857 14.8571C9.26857 14.8571 10.96 14.1829 12.2629 13.0743L12.5714 13.3829V14.2857L18.2857 20L20 18.2857L14.2857 12.5714H13.3829L13.0743 12.2629C14.1829 10.96 14.8571 9.26857 14.8571 7.42857ZM2.28571 7.42857C2.28571 4.57143 4.57143 2.28571 7.42857 2.28571C10.2857 2.28571 12.5714 4.57143 12.5714 7.42857C12.5714 10.2857 10.2857 12.5714 7.42857 12.5714C4.57143 12.5714 2.28571 10.2857 2.28571 7.42857Z" fill="CurrentColor" />
-                    </svg>
+                    <SearchIcon className="w-5 h-5"/>
                 </button>
 
-                <button className="lg:flex hidden items-center justify-center w-12 h-12 rounded-xl bg-primary ml-4 p-3">
-                    <svg className="w-5 h-5 text-light" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M0 3V5.33333H20V3H0ZM3.33333 11.1667H16.6667V8.83333H3.33333V11.1667ZM12.2222 17H7.77778V14.6667H12.2222V17Z" fill="currentColor" />
-                    </svg>
-                </button>
+                <ModalProvider>
+                    <OpenModal>
+                    <button className="lg:flex hidden items-center text-light justify-center w-12 h-12 rounded-xl bg-primary ml-4 p-3">
+                        <SortingIcon className="w-5 h-5 " />
+                    </button>
+                    </OpenModal>
+                    <Modal className="fixed flex justify-end top-0 left-0 w-full h-full items-center z-40">
+                        <SearchFilterModal/>
+                    </Modal>
+                    
+                </ModalProvider>
+                
 
-                <button className="flex items-center justify-center w-12 h-12 rounded-xl bg-secondary ml-4 p-3">
-                    <svg className="h-5 w-5 text-light" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-                    </svg>
+                <button className="flex items-center justify-center text-light w-12 h-12 rounded-xl bg-secondary ml-4 p-3">
+                   <CartIcon className="w-5 h-5"/>
                 </button>
             </div>
 
