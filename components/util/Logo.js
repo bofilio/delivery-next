6f212110aea6f843b9d4f26c3d1f32b7fdd17e84
bofilio <hemzatalha@gmail.com>
2021-11-07 22:31:04 +0100

    add link to logo

--- components/util/Logo.js
+++ components/util/Logo.js
@@ -1,28 +1,32 @@
 import React from 'react'
-
-const Logo = ({variant,text_color}) => {
+import Link from 'next/link'
+const Logo = ({ variant, text_color }) => {
     const logo_img = "/img/hamburger.png";
-    switch (variant){
+    switch (variant) {
         case "big":
             return (
-                <div className="flex items-center">
-                    <img className="w-20 h-16 mr-4" src={logo_img} />
-                    <h1 className={ text_color+" text-4xl"}>Nibble</h1>
-                </div>
+                <Link href="/">
+                    <div className="flex items-center cursor-pointer">
+                        <img className="w-20 h-16 mr-4" src={logo_img} />
+                        <h1 className={text_color + " text-4xl"}>Nibble</h1>
+                    </div>
+                </Link>
             );
         case "normal":
             return (
-                <div className="flex items-center">
+                <Link href="/">
+                <div className="flex items-center cursor-pointer">
                     <img className=" w-10 h-8 mr-2" src={logo_img} />
-                    <h1 className={ text_color+"sm:block md:hidden lg:block block  text-lg font-bold"}>Nibble</h1>
+                    <h1 className={text_color + "sm:block md:hidden lg:block block  text-lg font-bold"}>Nibble</h1>
                 </div>
+                </Link>
             );
-            default: return(
-                <>
-                </>
-            )
+        default: return (
+            <>
+            </>
+        )
     }
-   
+
 }
 
 export default Logo
